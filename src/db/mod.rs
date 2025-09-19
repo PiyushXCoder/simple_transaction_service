db065de4efdac3334e9eaaf0998c17538b7377e6
PiyushXCoder <piyushxcoder@gmail.com>
2025-09-19 14:52:08 +0530

    refactor: allow dead_code

--- src/db/mod.rs
+++ src/db/mod.rs
@@ -2,4 +2,6 @@ pub mod account;
 pub mod api_keys;
 pub mod transaction;
 
+#[allow(dead_code)]
+#[async_trait::async_trait]
 trait DbStore: Send + Sync + account::Account + transaction::Transaction + 'static {}
